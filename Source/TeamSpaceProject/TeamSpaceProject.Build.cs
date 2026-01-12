2c37197b0c6b07ba349b66fca1ca562df3a31940
sangmin <sangsang366@gmail.com>
2026-01-12 09:52:02 +0900

    gameinstance subsystem->gameinstance로 세션 생성 object 바꿈

--- Source/TeamSpaceProject/TeamSpaceProject.Build.cs
+++ Source/TeamSpaceProject/TeamSpaceProject.Build.cs
@@ -12,7 +12,6 @@ public class TeamSpaceProject : ModuleRules
 
         PrivateDependencyModuleNames.AddRange(new string[] { });
 
-        DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
     }
 
 }
