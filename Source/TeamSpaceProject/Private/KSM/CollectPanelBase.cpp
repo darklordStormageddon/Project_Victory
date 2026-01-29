d0235b575308ec8a125515103659b1035ccaeb01
sangmin <sangsang366@gmail.com>
2026-01-29 14:32:02 +0900

    리튬 이미지 추가 + 도구 데미지, 내구도 연동

---
+++ Source/TeamSpaceProject/Private/KSM/CollectPanelBase.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "KSM/CollectPanelBase.h"
+#include "JHS/GameControl/StaticFunctionLibrary.h"
+#include "JHS/GameControl/JHSGameState.h"
+#include "Net/UnrealNetwork.h"
+
+// Sets default values
+ACollectPanelBase::ACollectPanelBase()
+{
+	bReplicates = true;
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ACollectPanelBase::BeginPlay()
+{
+	Super::BeginPlay();
+	AJHSGameState* _outGameState = nullptr;
+	if (!UStaticFunctionLibrary::TryGetGameState(_outGameState))
+		return;
+
+	_collectStateGroup = _outGameState->GetCollectStateGroup();
+}
+
+void ACollectPanelBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+		Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+		DOREPLIFETIME(ACollectPanelBase, _Tool_Type);
+		DOREPLIFETIME(ACollectPanelBase, _OutToolDamage);
+}
+
+// Called every frame
+void ACollectPanelBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACollectPanelBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void ACollectPanelBase::UpdateDurabilityAndDamage_Implementation()
+{
+	if (_collectStateGroup == nullptr)
+		return;
+
+	if (!_collectStateGroup->TrySelectTool(_Tool_Type))
+		return;
+
+	// 사용 예시
+	float DeltaTime = GetWorld()->GetDeltaSeconds();
+	if (!_collectStateGroup->TryUseTool(_Tool_Type, DeltaTime, _OutToolDamage))
+		return;
+}
+
