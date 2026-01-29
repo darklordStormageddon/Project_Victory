7bb00d5a92df3e3f05f4144fb7533ee71a46c6ad
최주현 <iprite359@gmail.com>
2026-01-29 11:42:21 +0900

    빌드용 배치 기반 다지기(배치 및 크기 설정)

--- Source/TeamSpaceProject/Private/YSH/TurretBase_AT2.cpp
+++ Source/TeamSpaceProject/Private/YSH/TurretBase_AT2.cpp
@@ -59,7 +59,7 @@ void ATurretBase_AT2::Tick(float DeltaTime)
 		return;
 
 	// 디버그 시각화
-	DrawDebugVisualization();
+	/*DrawDebugVisualization();*/
 
 	// 1. 타겟 탐색 및 추적
 	FindAndTrackTarget(DeltaTime);
